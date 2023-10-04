e4694336502c58adbecb1208456db66622ae00ad
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-04 19:33:54 +0000

    task 1 pack directory

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive from the contents """
+from fabric.api import local
+from datetime import datetime
+import re
+
+
+def do_pack():
+    date = datetime.now().isoformat()
+    date = re.findall('\\d+', date)
+    date = ''.join(date)
+    name = f'web_static_{date}.tgz'
+    local("mkdir -p versions")
+    local(f"tar -czvf ./versions/{name} ./web_static")
