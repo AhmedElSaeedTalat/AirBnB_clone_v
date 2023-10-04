ab4175d2d585ffb661e2c9f0b0819b9b6cd82dff
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-04 16:25:43 +0000

    task 0 setup nginx

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# script to install nginx and add directories
+
+
+sudo apt-get -y update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+content='<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>'
+echo "$content" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+to_replace='# pass PHP scripts to FastCGI server'
+replace_with='location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}'
+sudo sed -i "s|$to_replace|$replace_with|g" /etc/nginx/sites-available/default
+sudo service nginx restart
