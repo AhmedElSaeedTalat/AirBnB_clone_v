d76a8e7649ccdc563da51d568f65a50b72832972
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-20 19:01:57 +0000

    t0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home():
-    """ home(): function to route to home"""
+    """ home(): function to route to home """
     return 'Hello HBNB!'
 
 
