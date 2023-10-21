c50e36c26d1bcb23f1baf2de8f430b11e2f378d0
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-21 21:50:23 +0000

    t0

--- models/state.py
+++ models/state.py
@@ -10,13 +10,13 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    if hb_storage == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City",
-                              cascade="all, delete",
-                              passive_deletes=True,
-                              backref="state")
-    else:
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",
+                          cascade="all, delete",
+                          passive_deletes=True,
+                          backref="state")
+
+    if hb_storage != 'db':
         name = ""
 
         @property
